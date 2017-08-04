c4c5102b623317322feee8b513c50fc2f59901e3
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2017-08-04 16:15:57 +0700

    Remove assets precompile from the buildback

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -106,7 +106,7 @@ WARNING
         post_bundler
         create_database_yml
         install_binaries
-        run_assets_precompile_rake_task
+        # run_assets_precompile_rake_task
         run_load_assets_rake_task
       end
       best_practice_warnings
